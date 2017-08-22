d2b4053756585d10f9c13c50208ee82f6602ed16
JinfeiChen <jinfei_chen@icloud.com>
2017-08-22 17:56:19 +0800

    fremework

---
+++ YWSDK.framework/Versions/A/Headers/YWSDK.h
@@ -0,0 +1,13 @@
+//
+//  YWSDK.h
+//  Pods
+//
+//  Created by cjf on 2017/8/22.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YWSDK : NSObject
+
+@end
---
+++ YWSDK.framework/Versions/A/Headers/test.h
@@ -0,0 +1,13 @@
+//
+//  test.h
+//  Pods
+//
+//  Created by cjf on 2017/8/22.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface test : NSObject
+
+@end
